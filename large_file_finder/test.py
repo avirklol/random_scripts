0d54f575362cc763688c9383c6835a3a59fe98c6
Annand Virk <annandvirk@gmail.com>
2025-01-05 20:40:22 -0500

    Some small layout progress.

--- large_file_finder/test.py
+++ large_file_finder/test.py
@@ -1,26 +1,81 @@
+import os
+import platform
+import subprocess
+from pathlib import Path
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Input, Label, RichLog, LoadingIndicator
-from textual.containers import VerticalGroup, HorizontalGroup, VerticalScroll
-from find_largest_folders_and_files import analyze_path
+from textual.widgets import Header, Footer, Input, Label, RichLog, LoadingIndicator, Button
+from textual.containers import VerticalGroup, HorizontalGroup, VerticalScroll, Container, Horizontal, Vertical
+from textual.screen import Screen
+from textual.geometry import Size
+from large_file_finder import analyze_path
 
-class Widgets(VerticalGroup):
+config_path = Path.home()/'Library'
+
+# screen_size = Size(40, 60)
+
+class FolderLog(RichLog):
+
+    BORDER_TITLE = "FOLDERS"
 
     def compose(self) -> ComposeResult:
-        yield Label("poo")
-        yield Label("poo")
-        yield Label("poo")
-        yield Label("poo")
-        yield Label("poo")
+        yield RichLog(id="folder_log")
 
-class TestApp(App):
+class FileLog(RichLog):
+
+    BORDER_TITLE = "FILES"
+
+    def compose(self) -> ComposeResult:
+        yield RichLog(id="file_log")
+
+class Output(Vertical):
+
+    BORDER_TITLE = "OUTPUT"
+
+    def compose(self) -> ComposeResult:
+        yield FolderLog()
+        yield FileLog()
+
+class ConfigQuery(Vertical):
+
+    BORDER_TITLE = "CONFIG AND QUERY"
+
+    def compose(self) -> ComposeResult:
+        with Horizontal(id="config_query_header"):
+            yield Button("Select Path", id="path_select")
+            yield Label(f"Path: {config_path}", id="path")
+        with Vertical(id="config_query_body"):
+            yield RichLog(id="config_log")
+            yield Input(placeholder=">>>", id="query_input")
+
+class Widgets(Vertical):
+
+    BORDER_TITLE = "WIDGETS"
+
+    def compose(self) -> ComposeResult:
+        yield Label("Elapsed Scan Time", id="widget_1")
+        yield Label("Total GB", id="widget_2")
+        yield Label("BIG ASS APPS", id="widget_3")
+
+class TestApp(Screen):
 
     def compose(self) -> ComposeResult:
         yield Header(name="Header")
-        yield Widgets()
-        yield RichLog()
-        yield Input(placeholder="Input")
+        with Container (id="app_grid"):
+            yield Widgets(id='widgets')
+            yield Output(id='log')
+            yield ConfigQuery(id='config_query')
         yield Footer()
 
+class _app(App):
+
+    TITLE = "Large File Finder"
+    CSS_PATH = "test.tcss"
+    # size = screen_size
+
+    def on_ready(self):
+        self.push_screen(TestApp())
+
 if __name__ == '__main__':
-    app = TestApp()
+    app = _app()
     app.run()
+x
